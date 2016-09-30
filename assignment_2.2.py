a586c82c4d940792a92216452e33621c732a2160
seleznova <mary.kuzmenko@gmail.com>
2016-09-30 20:33:00 +0300

    Update exercise 2.3

--- assignment_2.2.py
+++ assignment_2.2.py
@@ -2,3 +2,20 @@
 
 name = raw_input("Enter your name")
 print "Hello %s" % name
+
+# Exercise 2.3
+
+# This first line is provided for you
+
+hrs = raw_input("Enter Hours:")
+
+# This is a rate
+
+rate = raw_input("Enter Rate:")
+
+hrs = float(hrs)
+rate = float(rate)
+
+gross_pay = hrs * rate
+
+print gross_pay
