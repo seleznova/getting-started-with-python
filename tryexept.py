9401d522f32ebf99c2e642560ecdbb48f0edba2b
seleznova <mary.kuzmenko@gmail.com>
2016-10-10 22:56:00 +0300

    Update tryexept.py

--- tryexept.py
+++ tryexept.py
@@ -14,3 +14,25 @@ except:
     istr = -1
 
 print 'Second', istr
+
+astr = 'Bobbik'
+try:
+    print 'Hello'
+    istr = int(astr)
+    print 'There'
+except:
+    istr = -1
+
+print 'Done', istr
+
+rawstr = raw_input('Enter a number:')
+
+try:
+    ival = int(rawstr)
+except:
+    ival = -1
+
+if ival > 0:
+    print 'Nice work'
+else:
+    print 'Not a number'
