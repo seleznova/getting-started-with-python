45aa3b34282f643825bf7b2e1014d274e9edf9d3
seleznova <mary.kuzmenko@gmail.com>
2016-10-11 19:14:04 +0300

    New task with assignment_try_except.py

---
+++ assignment_try_except.py
@@ -0,0 +1,24 @@
+# The code below almost works
+
+name = raw_input("Enter your name")
+print "Hello %s" % name
+
+# Exercise 2.3
+
+# This first line is provided for you
+
+hrs = raw_input("Enter Hours:")
+
+# This is a rate
+
+rate = raw_input("Enter Rate:")
+
+hrs = float(hrs)
+rate = float(rate)M
+
+if hrs <= 40:
+    gross_pay = hrs * rate
+else:
+    gross_pay = (hrs * rate) + ((hrs - 40) * (rate * 0.5))
+
+print gross_pay
